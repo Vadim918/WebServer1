493ea2374906e01648b16891b385914d5d117b7f
Vadim <multikvadimka1@mail.ru>
2019-09-16 21:50:27 +0300

    ready

---
+++ Vote.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebServer
+{
+    class Vote
+    {
+        public void Post(HttpListener listener, HttpListenerRequest req)
+        {
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest request = context.Request;
+            var response = context.Response;
+
+            if (req.HttpMethod == "POST")
+            {
+                using (Stream stream = request.InputStream)
+                {
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        Console.WriteLine(reader.ReadToEnd());
+                    }
+                }
+
+            }
+            
+
+        }
+    }
+}
